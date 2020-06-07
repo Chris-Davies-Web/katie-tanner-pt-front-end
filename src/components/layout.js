fc1d11f000f798301bde081ecf6bc0c62e2a53b8
Chris-Biglight <chris.davies@biglight.co.uk>
2020-06-07 09:36:21 +0100

    Test - removed header

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,11 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import "./global.scss"
-import Header from "./Header"
+//import Header from "./Header"
 import Footer from "./Footer"
 
 const Layout = ({ children }) => (
   <div className="layout">
-    <Header />
+    {/* <Header /> */}
     <div className="content">{children}</div>
     <Footer />
   </div>
