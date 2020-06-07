97fc9e8f80d90fc9f289ddf899ba896e488d5860
Chris-Biglight <chris.davies@biglight.co.uk>
2020-06-07 10:03:48 +0100

    Remade Header JS - issue was on GITHUB the Header component was called header.js - needs ot be a capital

--- src/components/header.js
+++ src/components/header.js
@@ -5,16 +5,15 @@ import MainMenu from "./MainMenu"
 import BurgerMenu from "./BurgerMenu"
 
 const Header = () => {
-  // const [headerFixed, setheaderFixed] = useState(false)
-  // useEffect(() => {
-  //   const onScroll = e => {
-  //     window.scrollY > 0 ? setheaderFixed(true) : setheaderFixed(false)
-  //   }
-  //   window.addEventListener("scroll", onScroll)
-  // })
+  const [headerFixed, setheaderFixed] = useState(false)
+  useEffect(() => {
+    const onScroll = e => {
+      window.scrollY > 0 ? setheaderFixed(true) : setheaderFixed(false)
+    }
+    window.addEventListener("scroll", onScroll)
+  })
   return (
-    // <header className={headerFixed ? "fixed" : ""}>
-    <header className="fixed">
+    <header className={headerFixed ? "fixed" : ""}>
       <div className="header-items">
         <BurgerMenu />
         <Logo />
